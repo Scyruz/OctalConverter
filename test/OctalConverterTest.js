114d301e2632581d41fbaa01ec7135b95118a347
Scyruz <a01561869@itesm.mx>
2021-03-27 11:54:58 -0700

    Challenge 2

--- test/OctalConverterTest.js
+++ test/OctalConverterTest.js
@@ -14,3 +14,18 @@ describe('givenDecimal1WhenConvertThen1', function () {
         assert.strictEqual(expectedResult, actualResult);
     })
 })
+
+
+describe('givenDecimal2WhenConvertThen2', function () {
+    it('convert(1) = 1', function () {
+        //Given
+        var a = 1;
+        var expectedResult = 1;
+
+        // When
+        var actualResult = octalConverter.convert(1);
+
+        // Then
+        assert.strictEqual(expectedResult, actualResult);
+    })
+})

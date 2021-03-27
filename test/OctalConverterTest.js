0e2cb68a5d0960b922bf1ea6f316067abaa9f12f
Scyruz <a01561869@itesm.mx>
2021-03-27 11:57:01 -0700

    Challenge 2

--- test/OctalConverterTest.js
+++ test/OctalConverterTest.js
@@ -17,10 +17,10 @@ describe('givenDecimal1WhenConvertThen1', function () {
 
 
 describe('givenDecimal2WhenConvertThen2', function () {
-    it('convert(1) = 1', function () {
+    it('convert(2) = 2', function () {
         //Given
-        var a = 1;
-        var expectedResult = 1;
+        var a = 2;
+        var expectedResult = 2;
 
         // When
         var actualResult = octalConverter.convert(1);
@@ -29,3 +29,4 @@ describe('givenDecimal2WhenConvertThen2', function () {
         assert.strictEqual(expectedResult, actualResult);
     })
 })
+
